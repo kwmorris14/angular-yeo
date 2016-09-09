e3e8fb0a13d78dc0b401bf90fc1c00c5d9413ae3
Keith Morris <kmorris@icct.com>
2016-09-09 10:17:00 -0400

    init2

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularYeoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularYeoApp
+ */
+angular.module('angularYeoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
